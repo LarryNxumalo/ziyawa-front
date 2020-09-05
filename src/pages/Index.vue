99294181118a72a243ffa7badc3a5a6cc1ad2973
Larry Nxumalo <larry.nxumalo@afrihost.com>
2020-09-05 02:08:56 +0200

    added events: allEvent for looping through v-cards :)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,22 +9,20 @@
               <v-tab>Item Three</v-tab>
             </v-tabs>
             <v-card
+            v-for="edge in $page.events.edges" :key="edge.node.id"
             max-width="400"
             >
+
               <v-img
                 class="white--text align-end"
                 height="200px"
-                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                :src="`http://localhost:1337${edge.node.thumbnail}`"
               />
+              <v-card-title>
+                {{ edge.node.title }}
+              </v-card-title>
 
-              <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-              <v-card-text class="text--primary">
-                <div>Whitehaven Beach</div>
-
-                <div>Whitsunday Island, Whitsunday Islands</div>
-              </v-card-text>
-
+              <v-card-subtitle class="pb-0">{{edge.node.date}}</v-card-subtitle>
               <v-card-actions>
                 <v-btn
                   color="orange"
@@ -50,7 +48,7 @@
 
 <page-query>
   query {
-  allEvent {
+  events: allEvent {
     edges {
       node {
         id
